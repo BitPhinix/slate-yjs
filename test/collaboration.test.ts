da2583f2fae810bd2cb3eb99bb1ea952c6869eaf
Kirk Johnson <kjohnson@hugo.team>
2020-10-06 12:14:06 -0600

    Change to use table-driven test pattern
    Also: update TestEditor to handle exceptions thrown within Promises.

--- test/collaboration.test.ts
+++ test/collaboration.test.ts
@@ -1,32 +1,109 @@
+import { Node, Operation } from 'slate';
 import { createEditor } from 'slate';
 import { TestEditor, withTest } from './testEditor';
 import { toSlateDoc } from '../src';
+import { createNode } from './utils';
 
-describe('test', () => {
-  it('test', async () => {
-    // Create source document, make some edits, extract the resulting Yjs updates.
-    const src = withTest(createEditor());
-    await TestEditor.applySlateOpsToSlate(src, [
+// Partial copy of transforms from apply/apply.test.ts.
+//
+// Verbatim copies of 'merge_node', 'move_node', 'remove_node', 'set_node' and
+// 'split_node' sections currently cause tests to fail and need further
+// investigation.
+const transforms = [
+  [
+    'insert_text',
+    [createNode('paragraph', '')],
+    [
       {
-        type: 'insert_node',
-        path: [0],
-        node: { type: 'paragraph', children: [] },
-      },
-      {
-        type: 'insert_text',
         marks: [],
         offset: 0,
         path: [0, 0],
-        text: 'test',
+        text: 'Hello ',
+        type: 'insert_text',
       },
-    ]);
-    const updates = TestEditor.getCapturedYjsUpdates(src);
+      {
+        marks: [],
+        offset: 6,
+        path: [0, 0],
+        text: 'collaborator',
+        type: 'insert_text',
+      },
+      {
+        marks: [],
+        offset: 18,
+        path: [0, 0],
+        text: '!',
+        type: 'insert_text',
+      },
+    ],
+    [createNode('paragraph', 'Hello collaborator!')],
+  ],
+  [
+    'remove_text',
+    [createNode('paragraph', 'Hello collaborator!')],
+    [
+      {
+        offset: 11,
+        path: [0, 0],
+        text: 'borator',
+        type: 'remove_text',
+      },
+      {
+        offset: 5,
+        path: [0, 0],
+        text: ' colla',
+        type: 'remove_text',
+      },
+    ],
+    [createNode('paragraph', 'Hello!')],
+  ],
+  [
+    'insert_node',
+    [createNode()],
+    [
+      {
+        type: 'insert_node',
+        path: [1],
+        node: { type: 'paragraph', children: [] },
+      },
+      {
+        type: 'insert_node',
+        path: [1, 0],
+        node: { text: 'Hello collaborator!' },
+      },
+    ],
+    [createNode(), createNode('paragraph', 'Hello collaborator!')],
+  ]
+];
 
-    // Apply those Yjs updates against a different editor.
-    const dst = withTest(createEditor());
-    TestEditor.applyYjsUpdatesToYjs(dst, updates);
+describe('slate operations propagate between editors', () => {
+  transforms.forEach(([op, input, operations, output]) => {
+    it(`apply ${op} operations`, async () => {
+      // Create two editors.
+      const src = withTest(createEditor());
+      const dst = withTest(createEditor());
 
-    expect(toSlateDoc(dst.syncDoc)).toEqual(toSlateDoc(src.syncDoc));
-    expect(dst.children).toEqual(src.children);
+      // Set initial state for src editor, propagate changes to dst editor.
+      await TestEditor.insertSlateNodes(src, input as Node[], [0]);
+      let updates = TestEditor.getCapturedYjsUpdates(src);
+      await TestEditor.applyYjsUpdatesToYjs(dst, updates);
+
+      // Verify initial states.
+      expect(toSlateDoc(src.syncDoc)).toEqual(input);
+      expect(toSlateDoc(dst.syncDoc)).toEqual(input);
+      expect(src.children).toEqual(input);
+      expect(dst.children).toEqual(input);
+
+      // Apply test ops to src editor, propagate changes to dst editor.
+      await TestEditor.applySlateOpsToSlate(src, operations as Operation[]);
+      updates = TestEditor.getCapturedYjsUpdates(src);
+      await TestEditor.applyYjsUpdatesToYjs(dst, updates);
+
+      // Verify final states.
+      expect(toSlateDoc(src.syncDoc)).toEqual(output);
+      expect(toSlateDoc(dst.syncDoc)).toEqual(output);
+      expect(src.children).toEqual(output);
+      expect(dst.children).toEqual(output);
+    });
   });
 });
