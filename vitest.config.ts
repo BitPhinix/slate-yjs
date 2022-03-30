a3202bccd6fdaa4da912f32a3cbe851410e47013
BitPhinix <ericmeier@protonmail.com>
2022-03-31 00:51:27 +0200

    ci: fix codecov reporting

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-unresolved */
+import { defineConfig } from 'vitest/config';
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  test: {
+    coverage: {
+      reporter: ['json'],
+    },
+  },
+});
